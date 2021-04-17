6eaa4459bf0790e79bc976c4356a7cfa2be54704
iamtariqueanjum <tariqueanjummd9302@gmail.com>
2021-04-17 15:45:43 +0530

    Created a model of products app & registered it in admin.py

--- DjangoDemo/settings.py
+++ DjangoDemo/settings.py
@@ -36,6 +36,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third - Party
+
+    # Own
+    'products',
 ]
 
 MIDDLEWARE = [
