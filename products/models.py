dd1cb59b40dbce87e9b67c65916f0f22a5b6cb13
iamtariqueanjum <tariqueanjummd9302@gmail.com>
2021-05-01 23:33:10 +0530

    model forms -Product form added

--- products/models.py
+++ products/models.py
@@ -7,4 +7,4 @@ class Product(models.Model):
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(decimal_places=2, max_digits=10000)
     summary = models.TextField(blank= False, null= False) # blank is regarding the rendering the value is required or not while null is regarding the database value
-    featured = models.BooleanField()  # null=True, default=True
+    featured = models.BooleanField(default=False)  # null=True, default=True
