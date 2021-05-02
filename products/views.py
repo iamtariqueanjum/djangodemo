cfaadb8b1c563f9214e42c482ae02eff404c075c
iamtariqueanjum <tariqueanjummd9302@gmail.com>
2021-05-03 00:08:10 +0530

    raw html forms

--- products/views.py
+++ products/views.py
@@ -4,14 +4,24 @@ from .models import Product
 from .forms import ProductForm
 
 def product_create_view(request):
-    form = ProductForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-        form = ProductForm()
-    context= {
-        'form': form
-    }
+    #print(request.GET)
+    #print(request.POST)
+    if request.method == 'POST':
+        new_title=request.POST.get('title')
+        print(new_title)
+        #Product.objects.create(title=new_title)
+    context= {}
     return render(request, "products/product_create.html", context)
+
+# def product_create_view(request):
+#     form = ProductForm(request.POST or None)
+#     if form.is_valid():
+#         form.save()
+#         form = ProductForm()
+#     context= {
+#         'form': form
+#     }
+#     return render(request, "products/product_create.html", context)
 
 def product_detail_view(request):
     obj = Product.objects.get(id=1)
