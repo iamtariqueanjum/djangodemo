43971d7ef51839f445c6e75f354af15cc604bb54
iamtariqueanjum <tariqueanjummd9302@gmail.com>
2021-05-04 20:51:43 +0530

    delete action performed

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import Http404
 from .models import Product
 from .forms import ProductForm, RawProductForm
@@ -68,3 +68,13 @@ def product_detail_view(request, id):
         'object': obj
     }
     return render(request, "products/product_detail.html", context)
+
+def product_delete_view(request, id):
+    obj = get_object_or_404(Product, id=id)
+    if request.method == "POST":
+        obj.delete()
+        return redirect('../../')
+    context = {
+        "object":obj
+    }
+    return render(request, "products/product_delete.html", context)
