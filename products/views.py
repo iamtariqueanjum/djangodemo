0f6b235d47ac992ebaf37eb88270ec37e451b91d
iamtariqueanjum <tariqueanjummd9302@gmail.com>
2021-05-04 18:11:14 +0530

    Handling DoesNotExist Exception

--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 from .models import Product
 from .forms import ProductForm, RawProductForm
 
@@ -51,7 +51,12 @@ def initial_render_view(request):
     return render(request, "products/product_create.html", context)
 
 def product_detail_view(request, id):
-    obj = Product.objects.get(id=id)
+    # obj = Product.objects.get(id=id)
+    # obj = get_object_or_404(Product, id=id)
+    try:
+        obj = Product.objects.get(id=id)
+    except Product.DoesNotExist:
+        raise Http404
     context = {
         'title': obj.title,
         'description': obj.description,
