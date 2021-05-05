312f10d8cfc72a326e3304ba05fc7be4dec88e3f
iamtariqueanjum <tariqueanjummd9302@gmail.com>
2021-05-05 21:49:20 +0530

    raw detail class based view

--- courses/urls.py
+++ courses/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import my_fun_view,CourseView
+from .views import my_fun_view,CourseView,CourseDetailView
 
 app_name = 'courses'
 
 urlpatterns = [
-    path('',CourseView.as_view(), name='course-list'),
-    # path('',CourseView.as_view(template_name='contact.html'), name='course-list'),
     #path('', my_fun_view, name='courses-list'),
+    # path('',CourseView.as_view(template_name='contact.html'), name='course-list'),
+    path('',CourseView.as_view(), name='course-list'),
+    path('<int:id>',CourseDetailView.as_view(), name='course-detail'),
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,11 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
+
+from .models import Course
 
 # Create your views here.
 class CourseView(View):
     template_name = 'courses/about.html'
     def get(self, request, *args, **kwargs):
-        return render(request, self.template_name, {})
+        obj =  Course.objects.all()
+        context = {
+            "object":obj
+        }
+        return render(request, self.template_name, context)
+    # def post(request, *args, **kwargs):
+    #         return render(request, 'courses/about.html', {})
+
+class CourseDetailView(View):
+    template_name = 'courses/course_detail.html'
+    def get(self, request, id=None, *args, **kwargs):
+        context = {}
+        if id is not None:
+            obj = get_object_or_404(Course, id=id)
+            context['object'] = obj
+        return render(request, self.template_name, context)
     # def post(request, *args, **kwargs):
     #         return render(request, 'courses/about.html', {})
 
