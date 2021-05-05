b28cfc56bc40d9eae2915232f3f99ece0367c587
iamtariqueanjum <tariqueanjummd9302@gmail.com>
2021-05-05 21:58:59 +0530

    raw list class based view

--- courses/urls.py
+++ courses/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import my_fun_view,CourseView,CourseDetailView
+from .views import my_fun_view,CourseView,CourseDetailView,CourseListView,MyCourseListView
 
 app_name = 'courses'
 
 urlpatterns = [
     #path('', my_fun_view, name='courses-list'),
     # path('',CourseView.as_view(template_name='contact.html'), name='course-list'),
-    path('',CourseView.as_view(), name='course-list'),
+    path('',CourseListView.as_view(), name='course-list'),
+    # path('first',MyCourseListView.as_view(), name='course-list-first'),
     path('<int:id>',CourseDetailView.as_view(), name='course-detail'),
 ]
--- courses/views.py
+++ courses/views.py
@@ -23,8 +23,20 @@ class CourseDetailView(View):
             obj = get_object_or_404(Course, id=id)
             context['object'] = obj
         return render(request, self.template_name, context)
-    # def post(request, *args, **kwargs):
-    #         return render(request, 'courses/about.html', {})
+
+class CourseListView(View):
+    template_name = 'courses/course_list.html'
+    queryset = Course.objects.all()
+
+    def get_queryset(self):
+        return self.queryset
+
+    def get(self, request, id=None, *args, **kwargs):
+        context = {"object_list":self.get_queryset()}
+        return render(request, self.template_name, context)
+
+# class MyCourseListView(CourseListView):
+#     queryset = Course.objects.filter(id=1)
 
 def my_fun_view(request, *args, **kwargs):
     return render(request, 'courses/about.html', {})
