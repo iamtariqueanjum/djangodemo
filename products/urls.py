c37a0efc8aaa42f9c209a2e69035a7860d07e126
iamtariqueanjum <tariqueanjummd9302@gmail.com>
2021-05-05 11:14:30 +0530

    in-app urls & namespacing

--- products/urls.py
+++ products/urls.py
@@ -14,15 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
-from pages.views import home_view, contact_view, about_view, social_view
+from .views import product_detail_view,product_create_view,product_delete_view,product_list_view,product_update_view
 
+app_name = 'products'
 urlpatterns = [
-    path('products/', include('products.urls')),
-    path('', home_view, name='home'),
-    path('contact/', contact_view, name='contact-page'),
-    path('about/', about_view, name='about-page'),
-    path('social/', social_view, name='social-page'),
-    path('admin/', admin.site.urls),
+    path('', product_list_view, name='product-list'),
+    path('create/', product_create_view, name='product-create'),
+    # path('initial/', initial_render_view, name='product-create-initial'),
+    path('<int:id>/', product_detail_view, name='product-detail'),
+    path('<int:id>/update/', product_update_view, name='product-update'),
+    path('<int:id>/delete/', product_delete_view, name='product-delete'),
 ]
--- products/views.py
+++ products/views.py
@@ -3,6 +3,62 @@ from django.http import Http404
 from .models import Product
 from .forms import ProductForm, RawProductForm
 
+def product_create_view(request):
+    form = ProductForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        form = ProductForm()
+    context= {
+        'form': form
+    }
+    return render(request, "products/product_create.html", context)
+
+def product_update_view(request, id=id):
+    obj = get_object_or_404(Product, id=id)
+    form = ProductForm(request.POST or None, instance=obj)
+    if form.is_valid():
+        form.save()
+    context = {
+        'form': form
+    }
+    return render(request, "products/product_create.html", context)
+
+def product_list_view(request):
+    querySet = Product.objects.all()
+    context = {
+        "object_list": querySet
+    }
+    return render(request, "products/product_list.html", context)
+
+def product_detail_view(request, id):
+    # obj = Product.objects.get(id=id)
+    # obj = get_object_or_404(Product, id=id)
+    try:
+        obj = Product.objects.get(id=id)
+    except Product.DoesNotExist:
+        raise Http404
+    context = {
+        'title': obj.title,
+        'description': obj.description,
+        'price': obj.price,
+        'summary': obj.summary,
+        'featured': obj.featured
+    }
+    context= {
+        'object': obj
+    }
+    return render(request, "products/product_detail.html", context)
+
+def product_delete_view(request, id):
+    obj = get_object_or_404(Product, id=id)
+    if request.method == "POST":
+        obj.delete()
+        return redirect('../../')
+    context = {
+        "object":obj
+    }
+    return render(request, "products/product_delete.html", context)
+
 # def product_create_view(request):
 #     my_form = RawProductForm()
 #     if request.method == "POST":
@@ -27,61 +83,15 @@ from .forms import ProductForm, RawProductForm
 #     context= {}
 #     return render(request, "products/product_create.html", context)
 
-def product_create_view(request):
-    form = ProductForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-        form = ProductForm()
-    context= {
-        'form': form
-    }
-    return render(request, "products/product_create.html", context)
-
-def initial_render_view(request):
-    initial_data = {
-        'title':'This is default initial title'
-    }
-    obj = Product.objects.get(id=1)
-    form = ProductForm(request.POST or None, initial=initial_data, instance=obj)
-    if form.is_valid():
-        form.save()
-    context = {
-        'form': form
-    }
-    return render(request, "products/product_create.html", context)
-
-def product_detail_view(request, id):
-    # obj = Product.objects.get(id=id)
-    # obj = get_object_or_404(Product, id=id)
-    try:
-        obj = Product.objects.get(id=id)
-    except Product.DoesNotExist:
-        raise Http404
-    context = {
-        'title': obj.title,
-        'description': obj.description,
-        'price': obj.price,
-        'summary': obj.summary,
-        'featured': obj.featured
-    }
-    context= {
-        'object': obj
-    }
-    return render(request, "products/product_detail.html", context)
-
-def product_list_view(request):
-    querySet = Product.objects.all()
-    context = {
-        "object_list": querySet
-    }
-    return render(request, "products/product_list.html", context)
-
-def product_delete_view(request, id):
-    obj = get_object_or_404(Product, id=id)
-    if request.method == "POST":
-        obj.delete()
-        return redirect('../../')
-    context = {
-        "object":obj
-    }
-    return render(request, "products/product_delete.html", context)
+# def initial_render_view(request):
+#     initial_data = {
+#         'title':'This is default initial title'
+#     }
+#     obj = Product.objects.get(id=1)
+#     form = ProductForm(request.POST or None, initial=initial_data, instance=obj)
+#     if form.is_valid():
+#         form.save()
+#     context = {
+#         'form': form
+#     }
+#     return render(request, "products/product_create.html", context)
